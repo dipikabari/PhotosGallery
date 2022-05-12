2714e2d1b99b3c1bcb1a6329784245d5340092ab
Dipika Bari <dipikab@y7mail.com>
2022-05-12 16:19:24 +0100

    Initial commit

---
+++ PhotosGallery/ViewModel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosGallery
+//
+//  Created by Dipika Bari on 12/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
